5cc32a711fcdfabc7b952aed5057e5948a5ec36f
Bradley-Basik <114007065+Bradley-Basik@users.noreply.github.com>
2022-11-14 10:30:28 -0500

    Create Profile.js

---
+++ views/pages/Profile.js
@@ -0,0 +1,13 @@
+import { Text, View,Image } from "react-native";
+import pfp from "../assets/pfp.png"
+const Profile = () => {
+return (
+	<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+	<Text style={{  fontSize: 20 }}>Member since: N/A</Text>
+	 <Image source={pfp} style={{ width: 100, height: 100 }} />
+   <Text style={{ fontSize: 20 }}>Please login or create an account</Text>
+	</View>
+);
+};
+
+export default Profile;
